9e8aba31c8c55d9447b01ff523b3fbbe13095eff
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-03-26 12:43:17 +0200

    Added tab element

--- script.js
+++ script.js
@@ -32,3 +32,70 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords.top);
+  console.log(`window.scrollY ${window.scrollY}`);
+
+  // window.scrollTo(0, 100);
+
+  // Scrolling
+  // Old way
+  // window.scrollTo({
+  //   left: s1cords.left,
+  //   top: s1cords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // New way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+const h1 = document.querySelector('h1');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => {
+    t.classList.remove('operations__tab--active');
+  });
+  tabsContent.forEach(tab => {
+    tab.classList.remove('operations__content--active');
+  });
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
